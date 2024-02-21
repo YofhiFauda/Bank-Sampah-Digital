8f8235d099939c80faff1dcb9926124b99531a29
YofhiFauda <yofhi132@gmail.com>
2024-02-21 15:06:16 +0700

    Update Splash Screen

--- app/src/main/java/com/sampah/banksampahdigital/presentation/common/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/sampah/banksampahdigital/presentation/common/splashscreen/SplashScreenActivity.kt
@@ -1,14 +1,35 @@
 package com.sampah.banksampahdigital.presentation.common.splashscreen
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.sampah.banksampahdigital.R
+import android.os.Handler
+import android.view.View
+import com.sampah.banksampahdigital.databinding.ActivitySplashScreenBinding
+import com.sampah.banksampahdigital.presentation.common.onboarding.OnboardingActivity
 
+@Suppress("DEPRECATION")
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+    private val SPLASH_TIMEOUT = 2000L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.progressBar.visibility = View.VISIBLE
+
+        Handler().postDelayed({
+            binding.progressBar.visibility = View.GONE
+            val intent = Intent(this, OnboardingActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIMEOUT)
+
     }
 }
