9c4ca27644727f7afe5eb2250494ee0950836f06
YofhiFauda <yofhi132@gmail.com>
2024-06-08 00:46:04 +0700

    Update AppBar

--- app/src/main/java/com/sampah/banksampahdigital/MainActivity.kt
+++ app/src/main/java/com/sampah/banksampahdigital/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import com.sampah.banksampahdigital.common.onboarding.OnboardingActivity
 import androidx.core.view.forEach
 import com.google.firebase.auth.FirebaseAuth
@@ -31,6 +33,11 @@ class MainActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
         firestore = FirebaseFirestore.getInstance()
+
+        val window = this.window
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.greenDark)
 
         setupNavController()
         setupBottomNavigationView()
